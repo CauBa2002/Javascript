a05ffa2d6cff8a8142b1d31ac3d28952e4451f4a
unknown <dattran0204@gmail.com>
2023-04-26 09:16:29 +0700

    Day1

---
+++ Buoi01/main.js
@@ -0,0 +1,10 @@
+var txt = "Trần Văn Đạt";
+// alert(txt)
+// console.log(txt) //Output ra console 1 chuỗi
+// console.warn(txt) //Output ra console 1 cảnh báo
+// console.error(txt) //Output ra console 1 lỗi
+// confirm('Xác nhận trên 18 tuổi') //Output ra thông báo trong tab
+setTimeout(function(){
+    // alert('Thông Báo')
+    console.log('Đây là log ' + Math.random())
+}, 2000)
